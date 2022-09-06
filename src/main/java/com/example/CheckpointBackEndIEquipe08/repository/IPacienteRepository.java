50248efb20e6465046ae3160da74ecf3cb17927f
marcelo.muniz <marcelo_mbg@hotmail.com>
2022-09-06 20:33:43 -0300

    Criada interfaces.

---
+++ src/main/java/com/example/CheckpointBackEndIEquipe08/repository/IPacienteRepository.java
@@ -0,0 +1,10 @@
+package com.example.CheckpointBackEndIEquipe08.repository;
+
+import com.example.CheckpointBackEndIEquipe08.entity.DentistaEntity;
+import com.example.CheckpointBackEndIEquipe08.entity.PacienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPacienteRepository extends JpaRepository<PacienteEntity, Integer> {
+}
