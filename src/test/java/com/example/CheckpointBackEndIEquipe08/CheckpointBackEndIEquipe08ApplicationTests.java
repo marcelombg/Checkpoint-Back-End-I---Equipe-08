e6e4aeeb92a4fd386e3d2044112c3f76d1023ca2
Dumith Bou Habib <dumith.couto@sensedata.com.br>
2022-09-20 19:06:06 -0300

    Begun testing - error in constructor

--- src/test/java/com/example/CheckpointBackEndIEquipe08/CheckpointBackEndIEquipe08ApplicationTests.java
+++ src/test/java/com/example/CheckpointBackEndIEquipe08/CheckpointBackEndIEquipe08ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.CheckpointBackEndIEquipe08;
 
+import com.example.CheckpointBackEndIEquipe08.entity.DentistaEntity;
+import com.example.CheckpointBackEndIEquipe08.entity.dto.DentistaDTO;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CheckpointBackEndIEquipe08ApplicationTests {
+    DentistaDTO dentista1 = new DentistaDTO("Dumith","Habib",12345);
 
-	/*@Test
-	void contextLoads() {
-	}*/
-
+    @Test
+    void testenome(){
+        Assert.assertEquals("Dumith", dentista1.getNome());}
 }
