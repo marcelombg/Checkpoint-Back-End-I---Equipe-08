7bd6f66ff877dea7b9d86284540da3d68587cb09
Dumith Bou Habib <dumith.couto@sensedata.com.br>
2022-09-20 21:56:03 -0300

    Begun testing - progress made

--- src/test/java/com/example/CheckpointBackEndIEquipe08/CheckpointBackEndIEquipe08ApplicationTests.java
+++ src/test/java/com/example/CheckpointBackEndIEquipe08/CheckpointBackEndIEquipe08ApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.CheckpointBackEndIEquipe08;
-
+import com.example.CheckpointBackEndIEquipe08.controller.DentistaController;
+import com.example.CheckpointBackEndIEquipe08.entity.DentistaEntity;
+import com.example.CheckpointBackEndIEquipe08.entity.dto.DentistaDTO;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CheckpointBackEndIEquipe08ApplicationTests {
 
-	/*@Test
-	void contextLoads() {
-	}*/
+    DentistaDTO dentista1 = new DentistaDTO(new DentistaEntity());
 
+    @Autowired
+    DentistaController dentistaController;
+
+    @Test
+    void name() {
+
+        //assertEquals(Boolean,dentistaController.buscarTodos().isEmpty());
+
+    }
+
+    @Test
+    void testeDumith(){
+        assertTrue(dentistaController.buscarTodos().isEmpty());
+
+    }
+
+
+    @Test
+    void testenome2(){
+//        assertEquals(dentistaController.buscarTodos().isEmpty());
+    }
 }
+
